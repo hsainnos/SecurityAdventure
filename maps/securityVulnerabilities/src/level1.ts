b5218b0f26ec8b1b428474368ef03acec5a48ae1
Mario Letsche <mario.letsche@gmail.com>
2022-05-18 09:16:19 +0200

    added zone context for kylo

--- maps/securityVulnerabilities/src/level1.ts
+++ maps/securityVulnerabilities/src/level1.ts
@@ -10,6 +10,42 @@ let quest_1: boolean = false;
 let quest_2: boolean = false;
 let quest_3: boolean = false;
 
+WA.room.hideLayer("Paper_Highlight");
+WA.room.hideLayer("Kylo_Highlight");
+
+WA.room.onEnterLayer("Paper_Highlight_Zone").subscribe(() => {
+    WA.room.showLayer("Paper_Highlight");
+});
+
+WA.room.onEnterLayer("Paper_Dehighlight_Zone").subscribe(() => {
+    WA.room.hideLayer("Paper_Highlight");
+});
+
+WA.room.onEnterLayer("Kylo_Highlight_Zone").subscribe(() => {
+    WA.room.showLayer("Kylo_Highlight");
+});
+
+WA.room.onEnterLayer("Kylo_Dehighlight_Zone").subscribe(() => {
+    WA.room.hideLayer("Kylo_Highlight");
+});
+
+WA.room.onEnterLayer("F1_Notebook").subscribe( () => {
+    currentTriggerMessage = WA.ui.openPopup("popUp_1_notebook", TextFiles.f1_notebook, []);
+});
+
+WA.room.onLeaveLayer("F1_Notebook").subscribe(() => {
+    closeTriggerMessage()
+});
+
+
+
 elevator.setCurrentLevel("Level1.json");
 elevator.setMaxLevelAvailable(2);
+
+function closeTriggerMessage() {
+    if (currentTriggerMessage !== undefined) {
+        currentTriggerMessage.close();
+        currentTriggerMessage = undefined;
+    }
+}
 
