a6d72b51c9cf13792f2fff50d03e2a2b16fca60f
Mario Letsche <mario.letsche@hs-augsburg.de>
2022-06-03 09:53:29 +0200

    right script data

--- maps/securityVulnerabilities/src/level1.ts
+++ maps/securityVulnerabilities/src/level1.ts
@@ -1,7 +1,6 @@
 import {elevator} from "../../shared/elevator/elevator.js"
 import {TextFilesGerman} from "../../shared/TextFiles/TextFilesGerman.js";
 import {TextFilesEnglish} from "../../shared/TextFiles/TextFilesEnglish.js";
-import "./augh.ogg"
 
 let TextFiles: any = TextFilesGerman;
 let currentItem: any = undefined;
@@ -32,17 +31,6 @@ WA.room.onEnterLayer("Kylo_Dehighlight_Zone").subscribe(() => {
 
 WA.room.onEnterLayer("F1_Notebook").subscribe( () => {
     currentTriggerMessage = WA.ui.openPopup("popUp_1_notebook", TextFiles.f1_notebook, []);
-    const mySound = WA.sound.loadSound("./src/augh.ogg")
-    const config = {
-        volume : 0.5,
-        loop : false,
-        rate : 1,
-        detune : 1,
-        delay : 0,
-        seek : 0,
-        mute : false
-    }
-    mySound.play(config);
 });
 
 WA.room.onLeaveLayer("F1_Notebook").subscribe(() => {
